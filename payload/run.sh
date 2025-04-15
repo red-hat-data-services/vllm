479b050d4c7e16dd6252cf8d3ff2be596071c4fb
Christopher Kodama <ckodama@redhat.com>
2025-04-14 21:32:08 -0500

    fix typo in url in run.sh

--- payload/run.sh
+++ payload/run.sh
@@ -17,7 +17,7 @@ trap "rm -rf ${HOME}/.netrc release release.tar.gz" EXIT
 # WHEEL_RELEASE="2.20.55+vllm-cuda-ubi9-x86_64"
 
 # Gitlab project ID, etc should be static 
-WHEEL_RELEASE_ARTIFACTS="https://gitlab.com/api/v4/projects/68045055/packages/generic/rhelai-wheels/${WHEEL_RELEASE}/wheels-${WHEEL_RELEASE}.tar.gz"
+WHEEL_RELEASE_ARTIFACTS="https://gitlab.com/api/v4/projects/68045055/packages/generic/vllm-wheels/${WHEEL_RELEASE}/wheels-${WHEEL_RELEASE}.tar.gz"
 
 
 # NOTE - ensure that flashinfer is included in wheel thing
